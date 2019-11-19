69ffe3d0b7d70e2052cb8239b40f9df3c5e765ac
mikael <mikaelvg@gmail.com>
2019-11-19 11:00:51 +0800

    Added IP Checker in Index

--- src/main/java/ph/devops/student/controller/IndexController.java
+++ src/main/java/ph/devops/student/controller/IndexController.java
@@ -1,16 +1,24 @@
 package ph.devops.student.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @RestController
 @RequestMapping("/")
 public class IndexController {
 
+    @Autowired
+    Environment environment;
+
     @GetMapping
-    public String sayHello() {
-        return "Hello and Welcome to Mikael's Student API App!";
+    public String sayHello() throws UnknownHostException {
+        return "Welcome to Mikael's Student API App! \n Your IP is: " + InetAddress.getLocalHost().getHostAddress();
     }
 }
